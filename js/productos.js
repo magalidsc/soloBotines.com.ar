cc007778a2a50491a4945bed56caafe39d06894d
Magali de Sousa <m.desousacandeias@gmail.com>
2021-10-05 20:55:35 -0300

    Mi primer commit

---
+++ js/productos.js
@@ -0,0 +1,104 @@
+// Productos disponibles en el e-commerce
+
+const productsAvailable = [
+
+    {
+        productName: "BOTINES DE FUTBOL ADIDAS GOLETTO 7 TF TURQUESA",
+        description: "Estos botines de fútbol presentan un exterior sintético resistente y liviano que brinda una sensación de ligereza a tus pies",
+        price: 10000,
+        brand: "ADIDAS",
+        type: "FUTBOL",
+        imgSrc: "img/botines-de-futbol-adidas-goletto-7-tf-turquesa-100010fy3542001-1.jpeg",
+        id: 1,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE FUTBOL UMBRO CAMPO FIFTY 3 FG GRIS",
+        description: "Suela liviana y flexible, con tapones y áreas especííficas de flexión.",
+        price: 10500,
+        brand: "UMBRO",
+        type: "FUTBOL",
+        imgSrc: "img/umbro.jpeg",
+        id: 2,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE FUTBOL PUMA FUTURE Z 4.1 NJR TF BLANCO",
+        description: "La colección está inspirada en la orgullosa herencia y el amor de Neymar Jr. por su país natal, Brasil.",
+        price: 10000,
+        brand: "PUMA",
+        type: "FUTBOL",
+        imgSrc: "img/puma_future.jpeg",
+        id: 3,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE FUTBOL ADIDAS NEMEZIZ MESSI 19.4 TF NIÑO NIÑA AZUL",
+        description: "Estos botines incorporan un exterior sintético suave con el logo distintivo de Lionel Messi.",
+        price: 12000,
+        brand: "ADIDAS",
+        type: "FUTBOL",
+        imgSrc: "img/nemezis.jpeg",
+        id: 4,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE FUTBOL NIKE MERCURIAL VAPOR 13 ACADEMY TF NARANJA",
+        description: "La suela exterior de goma está diseñada para proporcionar tracción multidireccional en el césped.",
+        price: 11000,
+        brand: "NIKE",
+        type: "FUTBOL",
+        imgSrc: "img/mercurial.jpeg",
+        id: 5,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE HOCKEY ADIDAS DIVOX 1.9S TF NARANJA",
+        description: "Incorpora una bota interior que abraza el pie y está reforzada con revestimientos que protegen contra el agua.",
+        price: 11999,
+        brand: "ADIDAS",
+        type: "HOCKEY",
+        imgSrc: "img/botines-de-hockey-adidas-divox-1-9s-tf-naranja-100010g25953001-1.jpeg",
+        id: 6,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE HOCKEY TOPPER FLICK 2 NIÑA GRIS",
+        description: "Calzado de hockey que aporta sujecion lateral y refuerzo en puntera y talon.",
+        price: 5999,
+        brand: "TOPPER",
+        type: "HOCKEY",
+        imgSrc: "img/botines-de-hockey-topper-flick-2-ni_a-gris-800010089616001-1.jpeg",
+        id: 7,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE RUGBY ADIDAS MALICE SG AZUL",
+        description: "La suela ligera imprime agilidad a tus movimientos en césped natural húmedo.",
+        price: 11999,
+        brand: "ADIDAS",
+        type: "RUGBY",
+        imgSrc: "img/botines-de-rugby-adidas-malice-sg-azul-100010fu8216001-1.jpeg",
+        id: 8,
+        cantidad: 1
+    },
+
+    {
+        productName: "BOTINES DE RUGBY PUMA BORUSSIA H8 HG NEGRO",
+        description: "Este botín de rugby PUMA está confeccionado en materiales resistentes que te acompañan en cada momento de tus partidos y entrenamientos.",
+        price: 11999,
+        brand: "PUMA",
+        type: "RUGBY",
+        imgSrc: "img/botines-de-rugby-puma-borussia-h8-hg-negro-640010104654001-1.jpeg",
+        id: 9,
+        cantidad: 1
+    }
+
+]
---
+++ js/script.js
@@ -0,0 +1,319 @@
+//Array Carrito//
+let carrito = [];
+let total = 0;
+const DOMtotal = document.getElementById("total");
+const DOMcarrito = document.getElementById("listaCarrito");
+const DOMCards = document.getElementById("allCards");
+
+if (localStorage.getItem("carritoLs")) {
+  carrito = JSON.parse(localStorage.getItem("carritoLs"));
+  mostrarCarrito();
+}
+
+//Declaro variables
+let nombreCliente = "";
+let apellidoCliente = "";
+let mail = "";
+let edad;
+let pago;
+
+//Función agregarAlCarrito//
+function agregarAlCarrito(e) {
+  carrito.push(e.target.getAttribute("idCard"));
+  localStorage.setItem("carritoLs", JSON.stringify(carrito));
+  mostrarCarrito();
+  calcularTotal();
+}
+console.log(carrito);
+
+function mostrarCarrito() {
+  DOMcarrito.textContent = "";
+  const quitamosDuplicados = [...new Set(carrito)];
+
+  quitamosDuplicados.forEach((productoCarro) => {
+    const miItem = productsAvailable.filter((itemProducto) => {
+      return itemProducto.id === parseInt(productoCarro);
+    });
+    const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+      return itemId === productoCarro ? (total += 1) : total;
+    }, 0);
+    const nuevoItem = document.createElement("li");
+    const botonEliminar = document.createElement("button");
+    nuevoItem.classList.add("itemCarrito", "list-group-item");
+    nuevoItem.textContent = `Producto: ${miItem[0].productName} ${
+      miItem[0].brand
+    } Precio x ${numeroUnidadesItem} UNIDADES = $${
+      miItem[0].price * numeroUnidadesItem
+    }`;
+    botonEliminar.classList.add(
+      "btn",
+      "btn-secundary",
+      "boton-eliminar",
+      "btn-outline-dark"
+    );
+    botonEliminar.setAttribute("type", "button");
+    botonEliminar.textContent = "x";
+    botonEliminar.setAttribute("productoId", productoCarro.id);
+    botonEliminar.dataset.productoCarro = productoCarro;
+    botonEliminar.addEventListener("click", eliminarDelCarrito);
+    nuevoItem.appendChild(botonEliminar);
+    DOMcarrito.appendChild(nuevoItem);
+    calcularTotal();
+  });
+}
+
+//Función VACIAR CARRITO//
+$("#boton-vaciar").on("click", vaciarCarrito);
+
+function vaciarCarrito() {
+  if (carrito != undefined) {
+    //vacío el carrito
+    carrito.splice(0, carrito.length);
+    // llamo by id a la lista
+    $("#listaCarrito");
+    //saco elementos del dom
+    $("#listaCarrito").html("");
+    $("#total").html("");
+    localStorage.clear();
+    total = 0;
+    calcularTotal();
+  } else {
+    console.log("algo falló");
+  }
+}
+
+function eliminarDelCarrito(e) {
+  const idEliminado = e.target.dataset.productoCarro;
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== idEliminado;
+  });
+  localStorage.setItem("carritoLs", JSON.stringify(carrito));
+  mostrarCarrito();
+  calcularTotal();
+}
+
+//Función para Calcular el total//
+function calcularTotal() {
+  JSON.parse(localStorage.getItem("carritoLs"));
+  if (carrito.length == 0) {
+    DOMtotal.textContent = "0";
+  } else {
+    total = 0;
+    carrito.forEach((productItem) => {
+      const subtotal = productsAvailable.filter((itemsCards) => {
+        return itemsCards.id === parseInt(productItem);
+      });
+      total = total + subtotal[0].price;
+      DOMtotal.textContent = total.toFixed(2);
+    });
+  }
+}
+
+// Creación array
+const listaClientes = [];
+
+// // Creación de objeto cliente con parámetros obtenidos en variables dentro de la función chart
+
+class Cliente {
+  constructor(nombre, apellido, mail, pago) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.mail = mail;
+    this.pago = pago;
+  }
+}
+
+const miCliente1 = new Cliente(nombreCliente, apellidoCliente, mail, pago);
+
+let newCards = "";
+
+function createCards(filtro = "default") {
+  let newCards =
+    filtro !== "default" ?
+    productsAvailable.filter(
+      (productsAvailable) => productsAvailable.type == filtro
+    ) :
+    productsAvailable;
+
+  DOMCards.textContent = "";
+  newCards.forEach((productsAvailable) => {
+    const nodoPadre = document.createElement("div");
+    nodoPadre.classList.add("card", "col-lg-3", "misCards");
+    // Body
+    const miNodoCardBody = document.createElement("div");
+    miNodoCardBody.classList.add("card-body");
+    // Titulo
+    const miNodoTitle = document.createElement("h5");
+    miNodoTitle.classList.add("card-title");
+    miNodoTitle.textContent = `${productsAvailable.productName} - ${productsAvailable.description}`;
+    // Imagen
+    const miNodoImagen = document.createElement("img");
+    miNodoImagen.classList.add("img-fluid", "imgProducts");
+    miNodoImagen.setAttribute("src", productsAvailable.imgSrc);
+    // Precio
+    const miNodoPrecio = document.createElement("p");
+    miNodoPrecio.classList.add("card-text");
+    miNodoPrecio.textContent = "Precio ARS$ " + productsAvailable.price;
+    // Precio dolar
+    const miNodoPrecioDolar = document.createElement("p");
+    miNodoPrecioDolar.classList.add("card-text");
+    miNodoPrecioDolar.textContent =
+      "Precio USD " + (productsAvailable.price / dolarHoy).toFixed(2);
+    // Boton
+    const miNodoBoton = document.createElement("button");
+    miNodoBoton.classList.add("btn", "btn-outline-dark", "mt-auto");
+    miNodoBoton.textContent = "Agregar al Carrito";
+    miNodoBoton.setAttribute("idCard", productsAvailable.id);
+    miNodoBoton.addEventListener("click", agregarAlCarrito);
+    // Insertamos
+    miNodoCardBody.appendChild(miNodoImagen);
+    miNodoCardBody.appendChild(miNodoTitle);
+    miNodoCardBody.appendChild(miNodoPrecio);
+    miNodoCardBody.appendChild(miNodoPrecioDolar);
+    miNodoCardBody.appendChild(miNodoBoton);
+    nodoPadre.appendChild(miNodoCardBody);
+    DOMCards.appendChild(nodoPadre);
+  });
+}
+
+// ----------------------------------------------------------------------------------
+//Variables para el input
+let inputName = $("#inputName").val();
+let imputLastName = $("#inputLastName").val();
+let inputEmail = $("#inputLastName").val();
+let inputPaymentMethod = $("#inputPaymentMethod").val();
+
+let form = $("#buttonForm").on("click", function (e) {
+  console.log("Estoy escuchando el click");
+
+  inputName = $("#inputName").val();
+
+  imputLastName = $("#inputLastName").val();
+
+  inputEmail = $("#inputEmail").val();
+
+  inputPaymentMethod = $("#inputPaymentMethod").val();
+
+  console.log(inputName, imputLastName, inputEmail, inputPaymentMethod);
+
+  listaClientes.push(
+    new Cliente(inputName, imputLastName, inputEmail, inputPaymentMethod)
+
+
+  );
+
+  addProduct();
+
+
+  $(".btn-close").on("click", vaciarCarrito);
+  // $("#confirmacionDatos").append(`
+  //   <div>
+  //   <p>${listaClientes[0].nombre} ${listaClientes[0].apellido} Muchas gracias por tu compra!</p>
+  //   <p>Vas a estar recibiendo un mail en: ${listaClientes[0].mail} para coordinar el envío</p>
+  //   <p>Método de pago elegido: ${listaClientes[0].pago}</p>
+  //   </div>
+  //   `);
+
+  return listaClientes;
+});
+
+function carritoModal(e) {
+  if (carrito.length == 0) {
+    $("#ModalError").modal("show");
+  } else if (carrito.length != 0) {
+    $("#staticBackdrop").modal("show");
+  }
+}
+$("#btn-comprar").on("click", carritoModal);
+
+
+//función para formulario
+function addProduct() {
+  event.preventDefault();
+  if (
+    inputName == "" ||
+    imputLastName == "" ||
+    inputEmail == "" ||
+    inputPaymentMethod == ""
+  ) {
+    //si esta vacio sale y muestra un alert
+    alert("Por favor completa los campos vacios");
+    return false;
+  }
+  //Validamos que nombre no este vacio
+  if (inputName == "") {
+    //si esta vacio sale y muestra un alert
+    alert("Por favor completa tu nombre");
+    return false;
+  }
+
+  if (imputLastName == "") {
+    //si esta vacio sale y muestra un alert
+    alert("Por favor completa tu apellido");
+    return false;
+  }
+
+  if (inputEmail == "") {
+    //si esta vacio sale y muestra un alert
+    alert("Por favor completa tu mail");
+    return false;
+  }
+
+  if (inputPaymentMethod == "") {
+    //si esta vacio sale y muestra un alert
+    alert("Por favor completa tu metodo de pago");
+    return false;
+  }
+
+  $("#form").fadeOut(1000);
+  $("#boton-seguir").hide();
+  $("#confirmacionDatos").append(`
+    <div>
+    <p>${inputName} ${imputLastName} Muchas gracias por tu compra!</p>
+    <p>Vas a estar recibiendo un mail en: ${inputEmail} para coordinar el envío</p>
+    <p>Método de pago elegido: ${inputPaymentMethod}</p>
+    </div>
+    `);
+}
+
+
+
+// Acordeon
+
+$(function () {
+  $("#textHowToBuy,#textHowToPay").hide();
+
+  $("#howToBuy").on("click", function () {
+    $("#textHowToBuy").toggle().css("color", "rgb(40, 105, 245)");
+  });
+  $("#howToPay").on("click", function () {
+    $("#textHowToPay").toggle().css("color", "rgb(40, 105, 245)");
+  });
+});
+
+// Interactuar con API
+
+const urlApi =
+  "https://openexchangerates.org/api/latest.json?app_id=e82d0e43d7a545ba923feba46e1ec1d8";
+
+// let valorDolar;
+
+function traerDolar() {
+  $.get(urlApi, function (respuesta, estado) {
+    if (estado === "success") {
+      valorDolar = respuesta;
+      $("#cotizacion").append(`<div>
+                           <p>Dólar hoy: ${valorDolar.rates.ARS}</p>
+                          </div>`);
+    }
+
+    let dolarHoy = valorDolar.rates.ARS;
+
+    localStorage.setItem("miDolar", JSON.stringify(dolarHoy));
+  });
+}
+let dolarHoy = JSON.parse(localStorage.getItem("miDolar"));
+console.log(dolarHoy);
+traerDolar();
+
+createCards();
